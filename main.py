b437ea707ceb50068bd09e9af163fdc4bd025809
Eng.Omar <104109296+OmarAbusnad@users.noreply.github.com>
2023-10-06 00:44:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-# from fastapi import FastAPI, HTTPException
-# from pydantic import BaseModel
-# import uvicorn
-# from chatjpt import ArabicChatBot
-
-# app = FastAPI(title="Upload using FastAPI")
-
-# class ChatBotInput(BaseModel):
-#     query: str
-
-# chat_bot = ArabicChatBot()
-
-# @app.post("/get_response/{chat_input.query}")
-# async def get_chatbot_response(chat_input: ChatBotInput):
-#     try:
-#         response = chat_bot.run(chat_input.query)
-#         return {"response": response}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from chatjpt import ArabicChatBot
-g=ArabicChatBot()
-print(g.run("ماهي جامعة الرازي"))
+
+app = FastAPI(title="ChatBot using FastAPI")
+
+class ChatBotInput(BaseModel):
+    query: str
+
+chat_bot = ArabicChatBot()
+
+@app.post("/get_response/")
+async def get_chatbot_response(chat_input: ChatBotInput):
+    try:
+        response = chat_bot.run(chat_input.query)
+        return {"response": response}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"error": "Internal Server Error", "message": str(e)})
