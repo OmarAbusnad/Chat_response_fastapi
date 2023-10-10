7682841b13bc44dc300909ad4d6c1266e2807b4a
Eng.Omar <104109296+OmarAbusnad@users.noreply.github.com>
2023-10-11 01:28:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def process_data():
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=Flase,host='0.0.0.0', port=5000)
 
 
 
